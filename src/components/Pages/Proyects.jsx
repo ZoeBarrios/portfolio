7543511048695e6999e87c7d5212720c360e5a7c
ZoeBarrios <zbarrios@frsn.utn.edu.ar>
2024-02-05 17:19:31 -0300

    intentando corregir error de importacion

--- src/components/Pages/Proyects.jsx
+++ src/components/Pages/Proyects.jsx
@@ -1,5 +1,5 @@
 import { useCallback, useState } from "react";
-import Filter from "../Filter";
+import Filter from "../filter";
 import ProyectsList from "../ProyectsList";
 
 export default function Proyects() {
